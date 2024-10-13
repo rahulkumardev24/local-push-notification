1e3661314ce0f5e113dcb8533e835c3734f2c150
Rahul Kumar <rkrahulroy151617@gmail.com>
2024-10-13 17:45:10 +0530

    local push notification complete

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import 'notification_manager.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  NotificationHelper helper = NotificationHelper();
+
+  @override
+  void initState() {
+    super.initState();
+    helper.initializeNotificationManager();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(onPressed: (){
+
+          helper.notifyMessage(title: "New Message", body: "From Rahul ");
+
+        }, child: const Text("Push Notification")),
+      ),
+    );
+  }
+}
